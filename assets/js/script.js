fc7f2d97fd13bba5404d5929f490caa94298413a
jurba1997 <jurba2011@gmail.com>
2023-05-11 16:29:14 +0000

    add one question to the javascript

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+/**Questions */
+
+const questions = [{
+    question: "Which of these movies was directed by Quentin Tarantino?",
+    answers: [
+        {text: "The Green Mile", correct:false},
+        {text: "Unbreakable", correct:false},
+        {text: "The Shawshank Redemption", correct:false},
+        {text: "Pulp Fiction", correct:true},
+    ]
+}]
